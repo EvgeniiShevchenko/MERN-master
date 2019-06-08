50a0097d43cd5ddc8819347a279740518a16ddda
EvgeniiShevchenko <evgenii.shevchenko@outlook.com>
2019-06-08 19:06:08 +0300

    version 1.0.5

--- index.js
+++ index.js
@@ -42,24 +42,24 @@ app.use('/api/profile', profile);
 app.use('/api/post', post);
 
 
-//Heroku setup
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static('client/build'))
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+// Heroku setup
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 //Round 2 deploy
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'))
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')))
+//   //
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname = 'client/build/index.html'))
+//   })
+// }
 
 //Route Operations...
 app.get('/', (req, res) => {
